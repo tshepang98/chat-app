cf777d23a4f661685eddb885a901181dfca988f1
Tshepang Mathlore <tshepangmathlore@gmail.com>
2024-07-02 02:06:12 +0000

    adding routers and connect to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from './pages/Home/Home';
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/Signup/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
-import './App.css'
-import Login from './pages/Login/Login.jsx'
-import SignUp from './pages/Signup/Signup.jsx'
-
-export default function App() {
-  return (
-    <>
-      <div className='p-4 h-screen flex items-center justify-center'></div>
-      <SignUp/>
-    </>
-  )
+function App() {
+	const { authUser } = useAuthContext();
+	return (
+		<div className='p-4 h-screen flex items-center justify-center'>
+			<Routes>
+				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+			</Routes>
+			<Toaster />
+		</div>
+	);
 }
+
+export default App;
